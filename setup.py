b55fec07a7c39e2bbfc180726160bb11e6407316
ronnathaniel <rnathaniel7@gmail.com>
2022-01-30 16:14:57 -0500

    release: 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Author: Ron Nathaniel
 import setuptools
 
 PACKAGE = 'askquestions'
-VERSION = '0.0.3'
+VERSION = '0.1.2'
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open('requirements.txt') as f:
